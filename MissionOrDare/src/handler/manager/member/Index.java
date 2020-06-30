c46e2ad3b554e350f3c698eeb17d8bb03c963c8f
DESKTOP-DK8JJQC\Playdata <haerong22@gmail.com>
2020-06-30 17:47:25 +0900

    index.do 실행

--- MissionOrDare/src/handler/manager/member/Index.java
+++ MissionOrDare/src/handler/manager/member/Index.java
@@ -1,4 +1,4 @@
-package handler;
+package handler.manager.member;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,20 +7,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import handler.CommandHandler;
+
 @Controller
-public class DefaultHandler implements CommandHandler {
-	@RequestMapping( "*" )
+public class Index implements CommandHandler{
+	
+	@RequestMapping("/index")
 	@Override
 	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		request.setAttribute( "result", "처리할 수 없는 요청입니다." );
-		return new ModelAndView( "default" );
+		return new ModelAndView("manager/index");
 	}
+
 }
-
-
-
-
-
-
-
-
