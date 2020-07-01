f57e0418f46b6755e974ae0a20cf61339ec60370
HongYuChan <coocieco1@gmail.com>
2020-07-01 16:36:03 +0900

    Dto 추가수정

--- MissionOrDare/src/Dtos/UsersDto.java
+++ MissionOrDare/src/Dtos/UsersDto.java
@@ -22,5 +22,115 @@ public class UsersDto {
 	private String withdrawal_date;
 	private int score;
 	private String profile_picture;
+	public String getUser_nickname() {
+		return user_nickname;
+	}
+	public void setUser_nickname(String user_nickname) {
+		this.user_nickname = user_nickname;
+	}
+	public String getUser_email() {
+		return user_email;
+	}
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+	public String getUser_passwd() {
+		return user_passwd;
+	}
+	public void setUser_passwd(String user_passwd) {
+		this.user_passwd = user_passwd;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getAge_group() {
+		return age_group;
+	}
+	public void setAge_group(String age_group) {
+		this.age_group = age_group;
+	}
+	public Timestamp getDate_of_birth() {
+		return date_of_birth;
+	}
+	public void setDate_of_birth(Timestamp date_of_birth) {
+		this.date_of_birth = date_of_birth;
+	}
+	public String getKakao_birthday() {
+		return kakao_birthday;
+	}
+	public void setKakao_birthday(String kakao_birthday) {
+		this.kakao_birthday = kakao_birthday;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getIntresting1_llarge_category() {
+		return intresting1_llarge_category;
+	}
+	public void setIntresting1_llarge_category(String intresting1_llarge_category) {
+		this.intresting1_llarge_category = intresting1_llarge_category;
+	}
+	public String getIntresting1_2large_category() {
+		return intresting1_2large_category;
+	}
+	public void setIntresting1_2large_category(String intresting1_2large_category) {
+		this.intresting1_2large_category = intresting1_2large_category;
+	}
+	public String getIntresting1_3large_category() {
+		return intresting1_3large_category;
+	}
+	public void setIntresting1_3large_category(String intresting1_3large_category) {
+		this.intresting1_3large_category = intresting1_3large_category;
+	}
+	public String getIntresting1_4large_category() {
+		return intresting1_4large_category;
+	}
+	public void setIntresting1_4large_category(String intresting1_4large_category) {
+		this.intresting1_4large_category = intresting1_4large_category;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public String getSign_up_date() {
+		return sign_up_date;
+	}
+	public void setSign_up_date(String sign_up_date) {
+		this.sign_up_date = sign_up_date;
+	}
+	public String getWithdrawal_date() {
+		return withdrawal_date;
+	}
+	public void setWithdrawal_date(String withdrawal_date) {
+		this.withdrawal_date = withdrawal_date;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public String getProfile_picture() {
+		return profile_picture;
+	}
+	public void setProfile_picture(String profile_picture) {
+		this.profile_picture = profile_picture;
+	}
+	
+	
 	
 }
