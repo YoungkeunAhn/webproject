1b96f247b9d1691126f185638175461bf628b286
HongYuChan <coocieco1@gmail.com>
2020-07-08 09:31:26 +0900

    쀵

--- MissionOrDare/src/manager/supermanager/SupermanagerDao.java
+++ MissionOrDare/src/manager/supermanager/SupermanagerDao.java
@@ -10,13 +10,12 @@ public interface SupermanagerDao {
 
 	public int insertManager( ManagerDto managerDto );
 	
+	public int getCount();
 	public List<ManagerDto> managerDtos( Map<String, Integer> map );
 	
-	public int getCount();
-	
+	public int deleteArticle(String user_nickname);
 	public List<ManagerDto> findManager( Map<String, Object> mapp );
-	
 	public int getsearchManagerCount(String searchManager);
 
-	public int deleteArticle(String user_nickname);
+
 }
