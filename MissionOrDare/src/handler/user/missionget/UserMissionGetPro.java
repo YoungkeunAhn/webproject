a1ae8f1e024f66312342c94f050eae7c650882c6
DESKTOP-DK8JJQC\Playdata <haerong22@gmail.com>
2020-07-10 18:39:27 +0900

    핸들러생성

--- MissionOrDare/src/handler/user/missionget/UserMissionGetPro.java
+++ MissionOrDare/src/handler/user/missionget/UserMissionGetPro.java
@@ -1,5 +1,19 @@
 package handler.user.missionget;
 
-public class UserMissionGetPro {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import handler.CommandHandler;
+
+@Controller
+public class UserMissionGetPro implements CommandHandler{
+	@RequestMapping("/user_missionGetPro")
+	@Override
+	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return new ModelAndView("user/pages/user_missionGetPro");
+	}
 }
--- MissionOrDare/src/handler/user/userpage/UserMessagePro.java
+++ MissionOrDare/src/handler/user/userpage/UserMessagePro.java
@@ -1,5 +1,19 @@
 package handler.user.userpage;
 
-public class UserMessagePro {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import handler.CommandHandler;
+
+@Controller
+public class UserMessagePro implements CommandHandler{
+	@RequestMapping("/user_messagePro")
+	@Override
+	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return new ModelAndView("user/pages/user_messagePro");
+	}
 }
